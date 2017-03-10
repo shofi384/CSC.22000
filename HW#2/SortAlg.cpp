c60cba2c7b85224926b5e601513dbf790e4fd8b8
csc103 student <me@citymail.ccny.cuny.edu>
2017-03-09 23:31:09 -0500

    Updating HW#2

--- HW#2/SortAlg.cpp
+++ HW#2/SortAlg.cpp
@@ -39,12 +39,9 @@ int* MakeRevSeqArray(int n)
 	return a;
 }
 
-void StrInsSort(int n)
+void StrInsSort(int a[], int p, int r, int& CompCount, int& SwapCount)
 {
-	int CompCount=0, SwapCount=0;
-	for(int r=0; r<10; r++)
-	{
-	int* a = MakeRandArray(n);
+	int n = r-p+1;
 	for(int i=1; i<n; i++)
 	{
 		CompCount++;
@@ -56,8 +53,114 @@ void StrInsSort(int n)
 			i--;
 		}
 	}
+}
+
+void RepeatStrInsSort(int n)
+{
+	int CompCount=0, SwapCount=0;
+	for(int r=0; r<10; r++)
+	{
+	int* a = MakeRandArray(n);
+	StrInsSort(a, 0, n-1, CompCount, SwapCount);
 	}
-	cout<<"The average of insertion sorting "<<n<<" elements 10 times: \n   Comparison: "<<CompCount/10<<"\n   Swapping: "<<SwapCount/10<<endl;
+	cout<<"The average of Insertion sorting "<<n<<" elements 10 times: \n   Comparison: "<<CompCount/10<<"\n   Swapping: "<<SwapCount/10<<endl;
+}
+
+void merge(int a[], int p, int q, int r, int& CompCount)
+{
+	int n1=q-p+1;
+	int n2=r-q;
+	int L[n1+1];
+	int R[n2+1];
+	for(int i=1;i<=n1;i++)
+	{
+		L[i]=a[p+i-1];
+	}
+	for(int j=1;j<=n2;j++)
+	{
+		R[j]=a[q+j];
+	}
+
+	L[n1+1]=900000000;
+	R[n2+1]=999000000;
+
+	int i=1, j=1;
+	for(int k=p;k<=r;k++)
+	{
+		if(L[i]<=R[j])
+		{
+			CompCount++;
+			a[k]=L[i];
+			i=i+1;
+		}
+		else
+		{
+			a[k]=R[j];
+			j=j+1;
+		}
+	}
+}
+
+void MergeSort(int a[], int p, int r, int& CompCount)
+{
+	int q;
+	if(p<r)
+	{
+		CompCount++;
+		q=(p+r)/2;
+		MergeSort(a,p,q, CompCount);
+		MergeSort(a,q+1,r, CompCount);
+		merge(a,p,q,r, CompCount);
+	}
+}
+
+void RepeatMerge(int n)
+{
+	int CompCount=0;
+	for(int i =0; i<10; i++)
+	{
+		int* a = MakeRandArray(n);
+		MergeSort(a, 0, n-1, CompCount);
+	}
+	cout<<"The average of Merge sorting "<<n<<" elements 10 times: \n   Comparison: "<<CompCount/10<<endl;
+}
+
+void KMergeSort(int a[], int p, int r, int& CompCount, int& SwapCount)
+{
+	int q;
+	int n = r-p+1, k=20;
+	if(n>1)
+	{
+		CompCount++;
+
+	if(n<=k)
+	{
+		CompCount++;
+		StrInsSort(a, p, r, CompCount, SwapCount);
+	}
+		else
+		{
+			q=(p+r)/2;
+		KMergeSort(a,p,q, CompCount, SwapCount);
+		KMergeSort(a,q+1,r, CompCount, SwapCount);
+		merge(a,p,q,r, CompCount);}
+	}
+}
+
+void RepeatKMerge(int n)
+{
+	int CompCount=0, SwapCount=0;
+	for(int i =0; i<10; i++)
+	{
+		int* a = MakeRandArray(n);
+		KMergeSort(a, 0, n-1, CompCount, SwapCount);
+
+		for(int i=0; i<n; i++)
+		{
+			cout<<a[i]<<" ";
+		}
+	}
+	cout<<"The average of KMerge sorting "<<n<<" elements 10 times: \n   Comparison: "<<CompCount/10<<"\n   Swapping: "<<SwapCount/10<<endl;
 }
 
 main()
@@ -67,16 +170,24 @@ main()
 	int* seq6;
 	seq2 = MakeSeqArray(100);
 	seq4 = MakeSeqArray(10000);
-	seq6 = MakeSeqArray(1000000);
+	//seq6 = MakeSeqArray(1000000);
 
 	int* rev2;
 	int* rev4;
 	int* rev6;
 	rev2 = MakeRevSeqArray(100);
 	rev4 = MakeRevSeqArray(10000);
-	rev6 = MakeRevSeqArray(1000000);
+	//rev6 = MakeRevSeqArray(1000000);
 
-	StrInsSort(100);
-	StrInsSort(10000);
-	StrInsSort(1000000);
+	RepeatStrInsSort(100);
+	RepeatMerge(100);
+	RepeatKMerge(100);
+
+	/*RepeatStrInsSort(10000);
+	RepeatMerge(10000);
+	RepeatKMerge(10000);
+
+	RepeatStrInsSort1000000);
+	RepeatMerge(1000000);
+	RepeatKMerge(1000000);*/
 }
